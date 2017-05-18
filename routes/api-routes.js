39ee4b5fe06e47c2ff602c4eb083c6446cf25298
Taehyun Lim <taehyun.lim@gmail.com>
2017-05-18 13:36:56 -0700

    Refined API calls

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,10 +12,16 @@ var db = require("../models");
 // =============================================================
 module.exports = function(app) {
 
-  // GET ALL USERS
-  app.get("/api/users/", function(req, res) {
+  // GET USER(S)
+  app.get("/api/users/:userId?", function(req, res) {
+    // WHEN userId PARAMETER IS PRESENT, SPECIFY WHERE CONDITION FIRST
+    const where = {};
+    if (req.params.userId) {
+      where.id = req.params.userId;
+    }
     // USER hasMany PROGRESS; PROGRESS belongsTo COURSE
     db.User.findAll({
+      where,
       include: [
         {
           model: db.Progress,
@@ -44,7 +50,8 @@ module.exports = function(app) {
                   progress: progress.current_progress,
                   completed: progress.completed,
                   courseId: progress.Course.id,
-                  courseTitle: progress.Course.course_title
+                  courseTitle: progress.Course.course_title,
+                  courseLength: progress.Course.progress_length
                 })
             })
           })
@@ -53,55 +60,71 @@ module.exports = function(app) {
     });
   });
 
-  // GET ONE USER WHOSE USERID IS SPECIFIED
-  app.get("/api/users/:userId", function(req, res) {
-    // USER hasMany PROGRESS; PROGRESS belongsTo COURSE
-    db.User.findAll({
-      where: {
-        id: req.params.userId
-      },
+  // GET COURSES
+  app.get("/api/courses/:courseId?", function(req, res) {
+    // WHEN courseId PARAMETER IS PRESENT, SPECIFY WHERE CONDITION FIRST
+    const where = {};
+    if (req.params.courseId) {
+      where.id = req.params.courseId;
+    }
+    // COURSE hasMany REVEIW; REVIEW belongsTo USER
+    // COURSE hasMany PROGRESS; PROGRESS belongsTo USER 
+    db.Course.findAll({
+      where,
       include: [
-        {
-          model: db.Progress,
-          include: [
             {
-              model: db.Course
+              model: db.Review,
+              include: [
+                {
+                  model: db.User
+                }
+              ]
+            },
+            {
+              model: db.Progress,
+              include: [
+                {
+                  model: db.User
+                }
+              ]
             }
           ]
-        }
-      ]
     })
-    .then(data => {
-      const resObj = data.map(user => {
-        return Object.assign(
-          {},
-          {
-            userId: user.id,
-            username: user.username,
-            firstName: user.first_name,
-            password: user.password,
-            courses: user.Progresses.map(progress => {
-              return Object.assign(
-                {},
-                { 
-                  progressId: progress.id,
-                  progress: progress.current_progress,
-                  completed: progress.completed,
-                  courseId: progress.Course.id,
-                  courseTitle: progress.Course.course_title
-                })
-            })
-          })
-      })
-      res.json(resObj)
-    });
-  });
-
-  // GET ALL COURSES
-  app.get("/api/courses/", function(req, res) {
-    db.Course.findAll({})
     .then(function(data) {
-      res.json(data);
+      const resObj = data.map(course => {
+        return Object.assign(
+          {},
+          {
+            courseId: course.id,
+            courseTitle: course.course_title,
+            courseLength: course.progress_length,
+            reviews: course.Reviews.map(review => {
+              return Object.assign(
+                {},
+                { 
+                  reviewId: review.id,
+                  reviewTitle: review.review_title,
+                  reviewText: review.review_text,
+                  userId: review.User.id,
+                  username: review.User.username,
+                  firstName: review.User.first_name
+                })
+            }),
+            progresses: course.Progresses.map(progress => {
+              return Object.assign(
+                {},
+                {
+                  progressId: progress.id,
+                  userId: progress.User.id,
+                  username: progress.User.username,
+                  firstName: progress.User.first_name,
+                  progress: progress.current_progress,
+                  completed: progress.completed
+                })
+            })
+          })
+      });
+      res.json(resObj);
     });
   });
 
@@ -179,66 +202,11 @@ module.exports = function(app) {
                   completed: progress.completed
                 })
             })
-            // progressId: progress.id,
-            // progress: progress.current_progress,
-            // courseId: progress.Course.id,
-            // courseTitle: progress.Course.course_title,
-            // courseLength: progress.Course.progress_length,
-            // userId: progress.Course.User.id,
-            // username: progress.Course.User.username
           })
       });
       res.json(resObj)
     });
   });
-
-// .then(dbUser => {
-//   const resObj = dbUser.map(user => {
-//     return Object.assign(
-//       {},
-//       {
-//         username: user.username,
-//         first_name: user.first_name
-//       })
-//   });
-//   res.json(resObj);
-// });
-
-  // GET ALL REVIEWS OF SELECTED COURSE
-  app.get("/api/reviews/:courseId", function(req, res) {
-    db.Course.findOne({
-      where: {
-        id: req.params.courseId
-      },
-      include: [db.Review]
-    }).then(function(data) {
-      res.json(data);
-    });
-  });
-
-  // GET PROGRESSES OF SELECTED USER
-  app.get("/api/progresses/:userId", function(req, res) {
-    db.User.findOne({
-      where: {
-        id: req.params.userId
-      },
-      include: [
-        {
-          model: db.Progress,
-          include: [
-            {
-              model: db.Course
-            }
-          ]
-        }
-      ]
-    }).then(dbUser => {
-      res.json(dbUser);
-    });
-  });
-
-
-
 
   // HACK (DO NOT USE FOR FRONTEND)
   app.get("/api/register/:username/:password/:first_name", function(req, res) {
@@ -267,7 +235,19 @@ module.exports = function(app) {
     });
   });
 
-  // 
+  // // POST route for saving a new post
+  // app.post("/api/posts", function(req, res) {
+  //   console.log(req.body);
+  //   db.User.create({
+  //     username: req.body.username,
+  //     password: req.body.password,
+  //     first_name: req.body.first_name
+  //   })
+  //   .then(function(data) {
+  //     res.json(data);
+  //   });
+  // });
+
 
 
   // app.get("/api/authors/:id", function(req, res) {
@@ -293,17 +273,17 @@ module.exports = function(app) {
 
 
 
-  // Get route for returning posts of a specific category
-  app.get("/api/posts/category/:username", function(req, res) {
-    db.User.findAll({
-      where: {
-        username: req.params.username
-      }
-    })
-    .then(function(data) {
-      res.json(data);
-    });
-  });
+  // // Get route for returning posts of a specific category
+  // app.get("/api/posts/category/:username", function(req, res) {
+  //   db.User.findAll({
+  //     where: {
+  //       username: req.params.username
+  //     }
+  //   })
+  //   .then(function(data) {
+  //     res.json(data);
+  //   });
+  // });
 
   // // Get rotue for retrieving a single post
   // app.get("/api/posts/:id", function(req, res) {
@@ -317,41 +297,41 @@ module.exports = function(app) {
   //   });
   // });
 
-  // POST route for saving a new post
-  app.post("/api/posts", function(req, res) {
-    console.log(req.body);
-    db.User.create({
-      username: req.body.username,
-      password: req.body.password,
-      first_name: req.body.first_name
-    })
-    .then(function(data) {
-      res.json(data);
-    });
-  });
+//   // POST route for saving a new post
+//   app.post("/api/posts", function(req, res) {
+//     console.log(req.body);
+//     db.User.create({
+//       username: req.body.username,
+//       password: req.body.password,
+//       first_name: req.body.first_name
+//     })
+//     .then(function(data) {
+//       res.json(data);
+//     });
+//   });
 
-  // DELETE route for deleting posts
-  app.delete("/api/posts/:id", function(req, res) {
-    db.User.destroy({
-      where: {
-        id: req.params.id
-      }
-    })
-    .then(function(data) {
-      res.json(data);
-    });
-  });
+//   // DELETE route for deleting posts
+//   app.delete("/api/posts/:id", function(req, res) {
+//     db.User.destroy({
+//       where: {
+//         id: req.params.id
+//       }
+//     })
+//     .then(function(data) {
+//       res.json(data);
+//     });
+//   });
 
-  // PUT route for updating posts
-  app.put("/api/posts", function(req, res) {
-    db.User.update(req.body,
-      {
-        where: {
-          id: req.body.id
-        }
-      })
-    .then(function(data) {
-      res.json(data);
-    });
-  });
-};
+//   // PUT route for updating posts
+//   app.put("/api/posts", function(req, res) {
+//     db.User.update(req.body,
+//       {
+//         where: {
+//           id: req.body.id
+//         }
+//       })
+//     .then(function(data) {
+//       res.json(data);
+//     });
+//   });
+// };
